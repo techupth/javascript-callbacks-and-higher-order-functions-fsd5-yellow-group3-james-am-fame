864d8564dd6e4de2b1e6ea3134bb10c35cc9c39f
peerawet <peerawet1996@gmail.com>
2023-12-06 13:11:39 +0700

    finish exercise

--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,24 @@
 //Exercise #2: At Least Five Function
 
 function atLeastFive(array, operation) {
-  // Start coding here
+  let count = 0;
+  for (let score of array) {
+    let result = operation(score);
+    count += result;
+  }
+  if (count >= 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function operation(score) {
+  if (score >= 70) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -10,6 +27,7 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, operation);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, operation);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, operation);
+console.log(scoreRoom1Result, scoreRoom2Result, scoreRoom3Result);
